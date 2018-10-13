6f6475566a17f550b79e1a121a00fe776b603712
Patrick Santos <patrickspteixeira@gmail.com>
2018-10-13 11:30:28 -0300

    add widget cascade fx

--- src/components/Widgets/index.js
+++ src/components/Widgets/index.js
@@ -1,15 +1,21 @@
 import React from "react";
+import { CSSTransition } from "react-transition-group";
 
 import styles from "./index.module.css";
 import skills from "./skills";
 
-function Widgets() {
+function Widgets({ mounted }) {
   return (
-    <ul>
+    <ul className={styles.list}>
       {skills.map((item, index) => (
-        <li className={styles.widgetItem} key={index}>
-          <item.icon className={styles.widgetIcon} />
-        </li>
+        <CSSTransition in={mounted} timeout={0} classNames="fade" key={index}>
+          <li
+            className={`${styles.item} ${mounted ? styles.mounted : ""}`}
+            style={{ transitionDelay: `${index * 0.1}s` }}
+          >
+            <item.icon className={styles.widgetIcon} />
+          </li>
+        </CSSTransition>
       ))}
     </ul>
   );
--- src/routes/Skills/index.js
+++ src/routes/Skills/index.js
@@ -3,10 +3,18 @@ import React, { Component } from "react";
 import Widgets from "../../components/Widgets";
 
 class Skills extends Component {
+  state = {
+    mounted: false
+  };
+
+  componentDidMount() {
+    this.setState({ mounted: true });
+  }
+
   render() {
     return (
       <div className="page">
-        <Widgets />
+        <Widgets mounted={this.state.mounted} />
       </div>
     );
   }
