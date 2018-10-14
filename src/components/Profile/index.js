9cf5449c1cc7acb419ed7fa4c6c3b20ed75295d9
Patrick Santos <patrickspteixeira@gmail.com>
2018-10-14 20:34:28 -0300

    refactor profile component to be stateless

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -9,44 +9,37 @@ import SelectLanguage from "../SelectLanguage";
 import Rotating from "./svg/Rotating.jsx";
 import SocialMedia from "../SocialMedia";
 
-class Profile extends Component {
-  state = {
-    value: "pt-BR"
-  };
-  render() {
-    return (
-      <div className={styles.profileWrapper}>
-        <div className={styles.profileContainer}>
-          <img
-            className={styles.profileImage}
-            src={ProfileImage}
-            alt="Patrick`s Profile"
-          />
-          <div className={styles.profileGradient}>
-            <Rotating className={styles.profileRotating} />
-          </div>
-        </div>
-        <LanguageContext.Consumer>
-          {context => (
-            <div>
-              <SelectLanguage
-                value={context.state.language}
-                onChange={event => context.changeLanguage(event.target.value)}
-              />
-
-              <FormattedMessage id="title">
-                {text => <h1 className={styles.profileTitle}>{text}</h1>}
-              </FormattedMessage>
-              <FormattedMessage id="subtitle">
-                {text => <h1 className={styles.profileSubtitle}>{text}</h1>}
-              </FormattedMessage>
-              <SocialMedia />
-            </div>
-          )}
-        </LanguageContext.Consumer>
+const Profile = () => (
+  <div className={styles.profileWrapper}>
+    <div className={styles.profileContainer}>
+      <img
+        className={styles.profileImage}
+        src={ProfileImage}
+        alt="Patrick`s Profile"
+      />
+      <div className={styles.profileGradient}>
+        <Rotating className={styles.profileRotating} />
       </div>
-    );
-  }
-}
+    </div>
+    <LanguageContext.Consumer>
+      {context => (
+        <div className={styles.intro}>
+          <SelectLanguage
+            value={context.state.language}
+            onChange={event => context.changeLanguage(event.target.value)}
+          />
+
+          <FormattedMessage id="title">
+            {text => <h1 className={styles.profileTitle}>{text}</h1>}
+          </FormattedMessage>
+          <FormattedMessage id="subtitle">
+            {text => <h1 className={styles.profileSubtitle}>{text}</h1>}
+          </FormattedMessage>
+          <SocialMedia />
+        </div>
+      )}
+    </LanguageContext.Consumer>
+  </div>
+);
 
 export default Profile;
