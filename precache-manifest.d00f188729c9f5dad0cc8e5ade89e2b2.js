79c0e69df964b8e23c3a0ad279cdbf5d2ca70c80
Patrick Santos <patrickspteixeira@gmail.com>
2018-10-20 20:34:57 -0300

    Updates

---
+++ precache-manifest.d00f188729c9f5dad0cc8e5ade89e2b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "16df245104c5536b98fd01b4bc4ed289",
+    "url": "/static/media/profile.16df2451.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c20b4a60932390fad25d",
+    "url": "/static/js/main.c20b4a60.chunk.js"
+  },
+  {
+    "revision": "fd6e197133e485d15ded",
+    "url": "/static/js/1.fd6e1971.chunk.js"
+  },
+  {
+    "revision": "c20b4a60932390fad25d",
+    "url": "/static/css/main.bb8f94ac.chunk.css"
+  },
+  {
+    "revision": "fd6e197133e485d15ded",
+    "url": "/static/css/1.4e829197.chunk.css"
+  },
+  {
+    "revision": "fd3d9741d0855f4eb9148e8bcd36f7d4",
+    "url": "/index.html"
+  }
+];
